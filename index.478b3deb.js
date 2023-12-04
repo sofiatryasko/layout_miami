3f3254b86e280a4b2e76ee1d7bc0f46838c2e5a3
Sofiia Triasko <sofia13tryasko@gmail.com>
2023-12-04 20:03:52 +0200

    make build

---
+++ index.478b3deb.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("contact-us__form");e.addEventListener("submit",function(t){t.preventDefault(),window.alert("This form has been successfully submitted!"),e.reset()});
+//# sourceMappingURL=index.478b3deb.js.map
--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const contactForm = document.getElementById('contact-us__form');
-
-contactForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  window.alert('This form has been successfully submitted!');
-  contactForm.reset();
-});
